8c719751b259a3d835def8bb229d926d18c4dfe5
dannyhchan <dannyhchan@gmail.com>
2022-12-29 18:00:02 -0500

    update readme

--- demo_site/demo_site/settings.py
+++ demo_site/demo_site/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_api_key',
     'apis',
+    'rest_framework',
+    'rest_framework_api_key',    
 ]
 
 MIDDLEWARE = [
